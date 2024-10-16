d0d51b44519690fa8d704e0545e21a13eed0f43a
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-10-16 23:51:03 +0530

    added solution for exercise 4

--- 1-typescript-exercises/ts-playground/src/index.ts
+++ 1-typescript-exercises/ts-playground/src/index.ts
@@ -1,43 +1,42 @@
 interface User {
+    type: 'user';
     name: string;
     age: number;
     occupation: string;
 }
 interface Admin {
+    type: 'admin';
     name: string;
     age: number;
     role: string;
 }
 export type Person = User | Admin;
 export const persons: Person[] = [
-    {
-        name: 'Max Mustermann',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        name: 'Jane Doe',
-        age: 32,
-        role: 'Administrator'
-    },
-    {
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    },
-    {
-        name: 'Bruce Willis',
-        age: 64,
-        role: 'World saver'
-    }
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
 ];
+export function isAdmin(person: Person) : person is Admin{
+    return person.type === 'admin';
+}
+export function isUser(person: Person):person is User {
+    return person.type === 'user';
+}
 export function logPerson(person: Person) {
-    let additionalInformation: string;
-    if ('role' in person) {
+    let additionalInformation: string = '';
+    if (isAdmin(person)) {
         additionalInformation = person.role;
-    } else {
+    }
+    if (isUser(person)) {
         additionalInformation = person.occupation;
     }
     console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
 }
-persons.forEach(logPerson);
+console.log('Admins:');
+persons.filter(isAdmin).forEach(logPerson);
+console.log();
+console.log('Users:');
+persons.filter(isUser).forEach(logPerson);
+// In case you are stuck:
+// https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates
