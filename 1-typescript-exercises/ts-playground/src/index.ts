6f0478afffb5beb12d005fe05b079f232d16cef6
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-10-17 00:35:24 +0530

    added solution for exercise 5

--- 1-typescript-exercises/ts-playground/src/index.ts
+++ 1-typescript-exercises/ts-playground/src/index.ts
@@ -1,3 +1,26 @@
+/*
+
+Intro:
+
+    Time to filter the data! In order to be flexible
+    we filter users using a number of criteria and
+    return only those matching all of the criteria.
+    We don't need Admins yet, we only filter Users.
+
+Exercise:
+
+    Without duplicating type structures, modify
+    filterUsers function definition so that we can
+    pass only those criteria which are needed,
+    and not the whole User information as it is
+    required now according to typing.
+
+Higher difficulty bonus exercise:
+
+    Exclude "type" from filter criteria.
+
+*/
+
 interface User {
     type: 'user';
     name: string;
@@ -13,18 +36,41 @@ interface Admin {
 export type Person = User | Admin;
 export const persons: Person[] = [
     { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
-    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
-    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
-    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
+    {
+        type: 'admin',
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        type: 'user',
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        type: 'admin',
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    },
+    {
+        type: 'user',
+        name: 'Wilson',
+        age: 23,
+        occupation: 'Ball'
+    },
+    {
+        type: 'admin',
+        name: 'Agent Smith',
+        age: 23,
+        role: 'Administrator'
+    }
 ];
-export function isAdmin(person: Person) : person is Admin{
-    return person.type === 'admin';
-}
-export function isUser(person: Person):person is User {
-    return person.type === 'user';
-}
+export const isAdmin = (person: Person): person is Admin => person.type === 'admin';
+export const isUser = (person: Person): person is User => person.type === 'user';
 export function logPerson(person: Person) {
-    let additionalInformation: string = '';
+    let additionalInformation = '';
     if (isAdmin(person)) {
         additionalInformation = person.role;
     }
@@ -33,10 +79,22 @@ export function logPerson(person: Person) {
     }
     console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
 }
-console.log('Admins:');
-persons.filter(isAdmin).forEach(logPerson);
-console.log();
-console.log('Users:');
-persons.filter(isUser).forEach(logPerson);
+export function filterUsers(persons: Person[], criteria: Partial<Omit<User, 'type'>>): User[] {
+    return persons.filter(isUser).filter((user) => {
+        const criteriaKeys = Object.keys(criteria) as (keyof Omit<User, 'type'>)[];
+        return criteriaKeys.every((fieldName) => {
+            return user[fieldName] === criteria[fieldName];
+        });
+    });
+}
+console.log('Users of age 23:');
+filterUsers(
+    persons,
+    {
+        age: 23
+    }
+).forEach(logPerson);
+
 // In case you are stuck:
-// https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#partialtype
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#predefined-conditional-types
